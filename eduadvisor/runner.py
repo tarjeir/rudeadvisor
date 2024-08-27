33a7b46d65a8989545c48a16bea014725a54a6df
Tarjei Romtveit <8700195+tarjeir@users.noreply.github.com>
2024-08-27 15:08:20 +0200

    Remove the celery dependency in runner

--- eduadvisor/runner.py
+++ eduadvisor/runner.py
@@ -1,5 +1,4 @@
 import uvicorn
-import subprocess
 import typer
 
 app = typer.Typer()
@@ -11,22 +10,6 @@ def start_server():
     Start the Uvicorn server with FastAPI application.
     """
     uvicorn.run("eduadvisor.api:app", host="127.0.0.1", port=8000, reload=True)
-
-
-@app.command()
-def start_worker():
-    """
-    Start the Celery worker.
-    """
-    subprocess.run(
-        [
-            "celery",
-            "-A",
-            "eduadvisor.worker",
-            "worker",
-            "--loglevel=debug",
-        ]
-    )
 
 
 if __name__ == "__main__":
